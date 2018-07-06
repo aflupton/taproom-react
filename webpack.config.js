b97e4ce7a1a25fd0235a6c6b8fca61b4563bf433
andrew lupton <aflupton@gmail.com>
2018-07-06 12:53:48 -0700

    add change to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'Tap Room React',
+      title: 'Tap Room - React',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
