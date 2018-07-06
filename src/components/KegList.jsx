cdc4595ba78a4ed021c450e775728538876a9e07
andrew lupton <aflupton@gmail.com>
2018-07-06 11:36:51 -0700

    add refactored code to KegList component; adds props

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,63 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Keg from './Keg';
 
-
-var masterKegList = [
-  {
-    name: 'Ruby Zozzle',
-    brewery: 'Hi-Wheel',
-    type: 'Sparkling Wine & Grapefruit',
-    abv: '6.8%',
-    price: '7',
-    remaining: '20'
-  },
-  {
-    name: 'Tart N Juicy',
-    brewery: 'Epic',
-    type: 'Sour IPA',
-    abv: '4.5%',
-    price: '6',
-    remaining: '60'
-  },
-  {
-    name: 'Hamm\'s',
-    brewery: 'Miller/Coors',
-    type: 'American Lager',
-    abv: '4.7%',
-    price: '3',
-    remaining: '65'
-  },
-  {
-    name: 'Prismatic',
-    brewery: 'Ninkasi',
-    type: 'Juicy IPA',
-    abv:  '5.9%',
-    price: '6',
-    remaining: '75'
-  },
-  {
-    name: 'Juicy Haze',
-    brewery: 'New Belgium',
-    type: 'India Pale Ale',
-    abv:  '7.5%',
-    price: '6',
-    remaining: '18'
-  },
-  {
-    name: '8 Hop',
-    brewery: 'New Belgium',
-    type: 'Pale Ale',
-    abv:  '5.5%',
-    price: '6',
-    remaining: '58'
-  }
-];
-
-function KegList(){
+function KegList(props){
   return (
     <div>
       <hr/>
-      {masterKegList.map((keg, index) =>
+      {props.kegList.map((keg) =>
         <Keg
           name={keg.name}
           brewery={keg.brewery}
@@ -65,10 +14,19 @@ function KegList(){
           abv={keg.abv}
           price={keg.price}
           remaining={keg.remaining}
-          key={index}/>
+          formattedWaitTime={keg.formattedWaitTime}
+          currentRouterPath={props.currentRouterPath}
+          key={keg.id}
+          onKegSelection={props.onKegSelection} />
       )}
     </div>
   );
 }
+
+KegList.propTypes = {
+  kegList: PropTypes.array,
+  currentRouterPath: PropTypes.string,
+  onKegSelection: PropTypes.func
+};
 
 export default KegList;
