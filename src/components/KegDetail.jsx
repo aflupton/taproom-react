4502aaa7f83297c4218bc70b1c76a58987223c47
andrew lupton <aflupton@gmail.com>
2018-07-09 09:35:46 -0700

    add css changes to KegDetail comp

--- src/components/KegDetail.jsx
+++ src/components/KegDetail.jsx
@@ -13,8 +13,8 @@ function KegDetail(props){
           background-color: #e3adb5;
         }
       `}</style>
-      <h1>Beer: <strong>{props.selectedKeg.name}</strong> - Brewery: <strong>{props.selectedKeg.brewery}</strong></h1>
-      <h2>Tapped {props.selectedKeg.formattedWaitTime} ago</h2>
+      <h2>Beer: <strong>{props.selectedKeg.name}</strong> - Brewery: <strong>{props.selectedKeg.brewery}</strong></h2>
+      <h3>Tapped {props.selectedKeg.formattedWaitTime} ago</h3>
       <h4>Abv: {props.selectedKeg.abv}%</h4>
       <h4>Price: ${props.selectedKeg.price}</h4>
       <h4>Pints remaining in keg: {props.selectedKeg.remaining}</h4>
